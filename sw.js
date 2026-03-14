05cf169b650417fc1c9190a5703d277d696747e4
Oleksii Zhuravko <115608102+oleksii-zhuravko@users.noreply.github.com>
2026-03-14 12:42:11 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'scanner412-v12';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './quagga.min.js',
+  './icon412.png',
+  './beep.mp3'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+
+
+
+
+
